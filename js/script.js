9723555abc460f71d38329331ee8adc2412fc551
mehedihasanme01 <mehedihasanme01@gmail.com>
2017-12-14 13:17:34 +0600

    upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$('#img1').click(function () {
+
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src',imgSrcValue);
+
+});
+
+$('#img2').click(function () {
+
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src',imgSrcValue);
+
+});
+
+$('#img3').click(function () {
+
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src',imgSrcValue);
+
+});
+
+$('#img4').click(function () {
+
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src',imgSrcValue);
+
+});
+
+
+
+
+// function create() {
+//     var newHeadingElement = document.createElement('h1');
+//     var newParagraphElement = document.createElement('p');
+//
+//     newHeadingElement.innerHTML ="this is a heading.";
+//     newParagraphElement.innerHTML ="this is a paragraph.";
+//
+//     document.getElementById('div').appendChild(newHeadingElement);
+//     document.getElementById('div').appendChild(newParagraphElement);
+//
+// };
+//
+// document.getElementById('btn').onclick =function () {
+//     create();
+//
+// }
